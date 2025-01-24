75c31be4a788fb24e3909d8c20f4401caaf2fb5b
Ava Tran <ava31@usf.edu>
2025-01-23 23:30:12 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+//Task 1: Declaring Variables 
+let employeeName = "Tate McRae"; //Employee's Name
+const employeeID = 2; //employee's ID number 
+var isActive = true; //employee's active status
+
+//logging each variables value
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
